de30ccc5b9063c6693c1b73d93caa6fa8cc3a38c
shunmaru <programming.learn226@gmail.com>
2023-10-28 11:24:10 +0000

    update2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,22 +1,19 @@
-
-def loop(num)
-      if (num % 3 == 0) && (num % 5 == 0)
-           return "FizzBuzz"
-      elsif num % 3 == 0
-           return "Fizz"
-      elsif num % 5 == 0
-            return "Buzz"
-      else 
-            return num
-      end
-end
-
-def fizzbuzz(start, num_max)
-    (1..num_max).each do |number|
-      puts loop(number)
-    end
-end
-
 num_max = 100
-fizzbuzz(1,num_max)
+
+def fizzbuzz(n)
+  if n % 3 == 0 && n % 5 == 0
+    return "FizzBuzz"
+  elsif n % 3 == 0
+    return "Fizz"
+  elsif n % 5 == 0
+    return "Buzz"
+  else
+    return n
+  end
+end
+
+(1..num_max).each do |i|
+  result = fizzbuzz(i)
+  puts result
+end
 #上記引数に整数を入れる
